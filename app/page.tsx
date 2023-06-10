2a19528e1f03f59c1954c757f21323ff37ef174c
tranh253 <trunganh25397@gmail.com>
2023-06-10 19:54:07 +0300

    Adding component to index file

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
 
 const post = [
   { title: "Testing 1", excerpt: "Testing for the post" },
@@ -13,15 +14,15 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {post.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
+          {post.map((post) => (
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8"></div>
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </main>
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const PostCard = () => {
-  return <div>PostCard</div>;
+const PostCard = (post) => {
+  return (
+    <div>
+      {post.title}
+      {post.exerpt}
+    </div>
+  );
 };
 
 export default PostCard;
