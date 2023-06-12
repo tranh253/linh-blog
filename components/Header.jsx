d342384ee2563c21793c322ad9b7cdf4058afa0b
tranh253 <trunganh25397@gmail.com>
2023-06-12 21:01:50 +0300

    Styling Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Link from "next/link";
 
+const categories = [
+  { name: "React", slug: "react" },
+  { name: "Web Development", slug: "web-dev" },
+];
+
 const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left blobck">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white"></span>
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              GraphCMS
+            </span>
           </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((categorie) => (
+            <Link key={categorie.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {categorie.name}
+              </span>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
